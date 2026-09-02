ba63d501ca6d73bd31f3f95d179d016073aa77f4
Aman Kumar <amandsvv4006@gmail.com>
2026-09-03 03:59:25 +0530

    fix(url schema): url refinement itself exception-safe

--- backend/src/modules/urls/url.schema.ts
+++ backend/src/modules/urls/url.schema.ts
@@ -1,16 +1,24 @@
 import { z } from "zod";
 
 export const createUrlSchema = z.object({
-    originalUrl : z.url("A valid url is required").max(2048, "URL Must not exceed 2048 characters.").refine(
-      (url) => {
-        const protocol = new URL(url).protocol;
+  originalUrl: z.url("A valid url is required").max(2048, "URL Must not exceed 2048 characters.")
+  .refine(
+    (value) => {
+      try {
+        const url = new URL(value);
 
-        return protocol === "http:" || protocol === "https:";
-      },
-      {
-        message: "Only HTTP and HTTPS URLs are allowed",
-      },
-    )
+        return (
+          url.protocol === "http:" ||
+          url.protocol === "https:"
+        );
+      } catch {
+        return false;
+      }
+    },
+    {
+      message: "Invalid URL",
+    },
+  ),
 });
 
 export type createUrlInput = z.infer<typeof createUrlSchema>;
