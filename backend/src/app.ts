a9661cfdc09d2de3a5f315dbeb914fc4578697f0
Aman Kumar <130950548+Amandsvv@users.noreply.github.com>
2026-09-14 14:33:43 +0530

    fix(API): removed unnecessary API
    Removed unused '/hit' endpoint and added global error handler.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,26 +21,22 @@ app.use(cors({
 app.use(requestIdMiddleware);
 app.use(requestLoggerMiddleware)
 app.use(cookieParser());
-
 app.use(express.json({ limit : "10kb" }));
 
 app.get('/health', (req,res)=>{
     return res.status(200).json({success : 'ok', message : "Server is running"});
 });
 
-
 import UrlRoutes  from "./modules/urls/url.routes.js";
 import redirectRouter from "./modules/redirect/redirect.routes.js";
 import authRouter from "./modules/auth/auth.routes.js";
 import userUrlRouter from "./modules/users/urls/user-urls.route.js";
 
-
-app.get("/hit", (req, res) =>{
-    throw new Error("Hiited")
-})
 app.use("/api/v1/urls", UrlRoutes);
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/user", userUrlRouter)
 app.use("/", redirectRouter);
+
 app.use(globalErrorHandler);
+
 export default app;
